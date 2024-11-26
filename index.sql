ed4b6d5564fcffaf1d731f2b725a07ca433bb228
Don <don.puerto@hotmail.com>
2024-11-26 21:31:16 +0800

    ..

--- index.sql
+++
@@ -1,3 +0,0 @@
-CREATE INDEX IF NOT EXISTS idx_user_roles_user_id ON public.user_roles (user_id) WHERE deleted_at IS NULL;
-CREATE INDEX IF NOT EXISTS idx_user_roles_role_id ON public.user_roles (role_id) WHERE deleted_at IS NULL;
-CREATE INDEX IF NOT EXISTS idx_roles_role_type ON public.roles (role_type) WHERE deleted_at IS NULL;
